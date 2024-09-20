ee0f08c5c9982ef82259bbab546a444cb3cfc700
Александр <kozlov.alexander.63@gmail.com>
2024-09-20 12:51:40 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -2,20 +2,21 @@
 # Работа со словарями
 
 my_dict = {'Alex': 1963, 'Alena': 1967, 'Mary': 2004, 'Lola': 2000}
-
-print(my_dict.get('Alex'))
-print(my_dict.get('sasha', 'Нет такого в справочнике!'))
+print('Dict:', my_dict)
+print('Existing value:', my_dict.get('Alex'))
+print('Not existing value:', my_dict.get('sasha'))
 
 my_dict['Katy'] = 2011
 my_dict['Lisa'] = 2011
 print(my_dict)
 deleted_pair = my_dict.pop('Lola')
-print('Значение из удаленной пары:',deleted_pair)
+print('Deleted value:', deleted_pair)
+print('Modified dictionary:', my_dict)
 
 # Работа с множествами
 
-my_set = {1, 2, 3, 4, 4, 4}
-
-print(my_set)
-my_set.discard(2)
-print(my_set)
+print()
+my_set = {1, 2.5, 'Hello', 'Hello', True, False, False, 1, 4}
+print('Set:', my_set)
+my_set.discard(2.5)
+print('Modified set:', my_set)
